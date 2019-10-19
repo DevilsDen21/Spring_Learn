1aba062f30cca2aa25c58cf44b37ce00ad1a6432
Gaurav <gaurav.gini34@gmail.com>
2019-10-19 19:35:44 +0530

    example of singelton

--- src/main/java/com/udamy/spring/App.java
+++ src/main/java/com/udamy/spring/App.java
@@ -6,7 +6,10 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/udamy/beans/beans.xml");
         Second second = (Second) context.getBean("second");
+        Second second1 = (Second) context.getBean("second");
         second.print();
+        second1.setEmpId(222);
         System.out.println(second);
+        System.out.println(second1);
     }
 }
