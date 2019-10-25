dfd112f3ae65a4433bc4db6a33b314714b7797de
Gaurav <gaurav.gini34@gmail.com>
2019-10-25 20:57:14 +0530

    Initial setup done

--- src/main/java/com/udamy/spring/School.java
+++ src/main/java/com/udamy/spring/School.java
@@ -6,7 +6,9 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class School {
     public static void main(String[] args) {
         ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/udamy/beans/beans.xml");
-        SubjectClass subjectClass = (SubjectClass) context.getBean("subjectClass");
-        System.out.println(subjectClass);
+        SubjectClass mathClass = (SubjectClass) context.getBean("mathClass");
+        SubjectClass physicsClass = (SubjectClass) context.getBean("physicsClass");
+        System.out.println(mathClass);
+        System.out.println(physicsClass);
     }
 }
--- src/main/java/com/udamy/spring/SubjectClass.java
+++ src/main/java/com/udamy/spring/SubjectClass.java
@@ -12,6 +12,29 @@ public class SubjectClass {
         this.subject = subject;
     }
 
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
     @Override
     public String toString(){
         return subject + " Class will begin at "+ startTime + " and it will end at "+ endTime;
