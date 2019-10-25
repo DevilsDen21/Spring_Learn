d474b74c19438dfab1bc5ac14adf58a189d55ff9
Gaurav <gaurav.gini34@gmail.com>
2019-10-25 20:13:37 +0530

    deleting files and creating an environment for a more understandable example

--- src/main/java/com/udamy/spring/App.java
+++ src/main/java/com/udamy/spring/App.java
@@ -5,8 +5,5 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/udamy/beans/beans.xml");
-        Second second = (Second) context.getBean("second");
-        second.print();
-        System.out.println(second);
     }
 }
--- src/main/java/com/udamy/spring/FactoryC.java
+++
@@ -1,8 +0,0 @@
-package com.udamy.spring;
-
-public class FactoryC {
-    public Second createSecond(int id, String name){
-        System.out.println("Creating bean by factory");
-        return new Second(id, name);
-    }
-}
