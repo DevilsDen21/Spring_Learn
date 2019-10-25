a277f83732a1277bc28647da244d0150064a11b7
Gaurav <gaurav.gini34@gmail.com>
2019-10-25 20:19:48 +0530

    new project based on a school model

--- src/main/java/com/udamy/spring/App.java
+++
@@ -1,9 +0,0 @@
-package com.udamy.spring;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-
-public class App {
-    public static void main(String[] args) {
-        ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/udamy/beans/beans.xml");
-    }
-}
---
+++ src/main/java/com/udamy/spring/School.java
@@ -0,0 +1,7 @@
+package com.udamy.spring;
+
+public class School {
+    public static void main(String[] args) {
+
+    }
+}
